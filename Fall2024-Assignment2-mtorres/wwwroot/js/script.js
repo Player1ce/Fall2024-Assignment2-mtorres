167fe2d0a6ca73bae1b376a83fb8eaaae9c01387
Marcelo Torres <mtorres3@crimson.ua.edu>
2024-10-05 21:19:55 -0500

    site works!

--- Fall2024-Assignment2-mtorres/wwwroot/js/script.js
+++ Fall2024-Assignment2-mtorres/wwwroot/js/script.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 
+    var searched = false
     function apiSearch() {
         var params = {
             'q': $('#query').val(),
@@ -22,13 +23,47 @@ $(document).ready(function() {
                     results += `<p><a href="${data.webPages.value[i].url}">${data.webPages.value[i].name}</a>: ${data.webPages.value[i].snippet}</p>`;
                 }
 
-                $('#searchResults').html(results).dialog();
+                $('#searchResults').html(results).show();
+                
+                searched = true
             })
             .fail(function () {
                 alert('error');
             });
     }
+    
+    function goToFirst() {
+        if (searched) {
+            window.location.href = $('#searchResults').find('p:first a:first').attr("href");
+        }
+        else {
+            alert("No search to go to.");
+        }
+    }
+    
+    function getCurrentTime() {
+        var currentDate = new Date();
+        return currentDate.toLocaleTimeString();
+    }
+    
+    function displayTime() {
+        $('#time').html(getCurrentTime()).show()
+    }
+
+    document.getElementById("query").addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+            if (event.shiftKey) {
+                // Action for Shift+Enter
+            } else {
+                // Action for Enter
+                event.preventDefault(); // Prevent the default action (form submission)
+                apiSearch();
+            }
+        }
+    });
 
     $('#searchButton').click(apiSearch)
+    $('#timeButton').click(displayTime)
+    $('#luckyButton').click(goToFirst)
 
 });
